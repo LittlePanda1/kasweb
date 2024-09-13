b4fec43d6b31339bee3f5e749780e46b1a28ceb7
LittlePanda1 <guruhpandhu01@gmail.com>
2024-09-13 22:36:55 +0700

    2.0

---
+++ js/script.js
@@ -0,0 +1,35 @@
+const userInfo = {
+    usn: "CutiePie",
+    pw: "24122023"
+}
+const ans1 = ["Gultik","Blok M", "Gultik Blok M","gultik","blok m","gultik blok m"];
+
+function loginFunc() {
+    let username = document.getElementById("usn").value;
+    let password = document.getElementById("pw").value;
+
+    if(username === "" || password === ""){
+        alert("Kotak tidak boleh kosong!");
+        }
+        else if(username !== userInfo.usn){
+            alert("Username anda salah!");
+        }
+        else if(password !== userInfo.pw) {
+            alert("Password anda salah!");
+        }
+        else{
+            alert("Selamat Anda Berhasil Login!!!");
+            let loginPage = document.getElementsByClassName("login-page")[0];
+            if(loginPage){
+                loginPage.style.display="none";
+            }
+
+           let home = document.getElementsByClassName("home")[0];
+
+           if(home) {
+            home.style.display="block";
+           }
+        }
+        }
+        
+    
