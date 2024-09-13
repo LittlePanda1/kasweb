24871ba8555287db73d18c1801669294064b8672
LittlePanda1 <guruhpandhu01@gmail.com>
2024-09-13 23:35:51 +0700

    3.0

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const userInfo = {
     pw: "24122023",
     usn: "cutiepie"
 }
-const ans1 = ["Gultik","Blok M", "Gultik Blok M","gultik","blok m","gultik blok m"];
+
 
 function loginFunc() {
     let username = document.getElementById("usn").value;
@@ -20,24 +20,50 @@ function loginFunc() {
         }
         else{
             alert("Selamat Anda Berhasil Login!!!");
-            let loginPage = document.getElementsByClassName("login-page")[0];
+            let loginPage = document.getElementsById("loginPage");
             if(loginPage){
                 loginPage.style.display="none";
             }
 
-           let home = document.getElementsByClassName("home")[0];
+           let home = document.getElementsById("homepage");
 
            if(home) {
             home.style.display="block";
            }
         }
 
-    document.getElementById("answer1").addEventListener("keydown", function(event){
-        if(event.key === "Enter") {
-            event.preventDefault();
-            document.getElementById("answerBtn").click();
         }
-    });
+
+        document.getElementById("answer1").addEventListener("keydown", function(event){
+            if(event.key === "Enter") {
+                event.preventDefault();
+                document.getElementById("answerBtn").click();
+            }
+        });
+        function sub1Func() {
+            let answer = document.getElementById("answer1").value.trim();
+            const ans1 = ["Gultik","Blok M", "Gultik Blok M","gultik","blok m","gultik blok m"];
+
+            if(ans1.includes(answer)){
+                alert("Betullll!!!");
+                let home = document.getElementById("homepage");
+                if(home){
+                    home.style.display="none";
+                }
+
+                let page1 = document.getElementById("page1");
+
+                if(page1){
+                    page1.style.display="block";
+                }
+            } else if(answer === ""){
+                alert("gaboleh kosong tw, coba jawab");
+            } else {
+                alert("Salah Wleee :p")
+               
+            }
+
+            
         }
         
     
